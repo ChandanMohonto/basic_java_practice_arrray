7f1c5162165442841fe5a0f1ac0ebdb75ca0f29d
Chandan Kumar Mahanta <75965142+ChandanMohonto@users.noreply.github.com>
2022-04-05 16:19:13 +0600

    Add files via upload

---
+++ Find_out_the_year_month_day.java
@@ -0,0 +1,30 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Find_out_the_year_month_day {
+    public static void main(String[] args) throws ParseException {
+
+            String thatDay = "25/12/2000 12:40:12 AM";
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss aa");
+            Date date1 = formatter.parse(thatDay);
+            Date date2 = new Date();
+            long d = date2.getTime() - date1.getTime();
+            long year = d / (1000l  * 60 * 60 * 24 * 365);
+            long day = d / (1000 * 60 * 60 * 24) % 365;
+            long month = (day / 30);
+            long day_2 = (day % 24);
+
+          //  long hour = (d / (1000 * 60 * 60)) % 24;
+           // long minutes = (d / (1000 * 60)) % 60;
+            //long second = (d / 1000) % 60;
+            System.out.println("Year " + year);
+            System.out.println("Month " + month);
+             System.out.println("day " + day_2);
+//            System.out.println("Day " + day);
+//            System.out.println("Hour " + hour);
+//            System.out.println("Minute " + minutes);
+//            System.out.println("Second " + second);
+        }
+    }
+
